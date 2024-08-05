9a4df0181413094232522d588146b93f16766805
navnee1h <95498140+navnee1h@users.noreply.github.com>
2024-08-05 23:50:04 +0530

    Create linked_list.cpp

---
+++ linked_list.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+// Node structure
+struct Node {
+    int data;
+    Node* next;
+};
+
+// Linked List class
+class LinkedList {
+private:
+    Node* head;
+
+public:
+    LinkedList() : head(nullptr) {}
+
+    // Add a node to the end of the list
+    void insert(int value) {
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = nullptr;
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            Node* temp = head;
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+    // Traverse and display the list
+    void traverse() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    ~LinkedList() {
+        while (head != nullptr) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    LinkedList list; // Create a linked list
+
+    list.insert(10); // Insert elements
+    list.insert(20);
+    list.insert(30);
+
+    list.traverse(); // Display list contents
+
+    return 0;
+}
